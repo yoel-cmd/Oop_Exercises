81859219ceb054001602b3e92e925953bfb590f9
yoel <yoeli5746@gmail.com>
2025-05-19 17:41:18 +0300

    I implemented 2 more tools in main and added them to the list and checked that their methods work.

--- Program.cs
+++ Program.cs
@@ -18,11 +18,16 @@ namespace Oop_Exercises
             DrillTool drill1 = new DrillTool("drill 1", "3.5");
             DrillTool drill2 = new DrillTool("drill 2", "4.4");
             HammerTool hammer1 = new HammerTool("hammer 1", "7.5");
+            NailGunTool nailGun = new NailGunTool("nailGun 1", "0.3");
+            WireStripperTool wire = new WireStripperTool("wire 1", "2.6");
 
             List<Tool> listTool = new List<Tool>();
             listTool.Add(drill1);
             listTool.Add(drill2);
             listTool.Add(hammer1);
+            listTool.Add(nailGun);
+            listTool.Add(wire);
+
 
             foreach (Tool item in listTool)
             {
