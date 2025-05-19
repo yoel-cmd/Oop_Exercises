67a93de62ba046ec43371ee956d2ef449aa2a83c
yoel <yoeli5746@gmail.com>
2025-05-19 17:32:45 +0300

    I took a class on nail clippers.

---
+++ NailGunTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class NailGunTool : Tool
+{
+    public NailGunTool(string name, string weight) : base(name, weight)
+    {
+        this._name = name;
+        this._weight = weight;
+    }
+
+    public override void Describe()
+    {
+        Console.WriteLine($"The name of the current is:{_name} tool is and its weight is: {_weight}");
+
+    }
+
+
+    public override void Use()
+    {
+        Console.WriteLine($"{_name}  He clipped fingernails and toenails. ");
+
+    }
+}
