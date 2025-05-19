b15c05053a9bfefd6933ef9690520d4e63551ebc
yoel <yoeli5746@gmail.com>
2025-05-19 17:05:19 +0300

    I added a pipe cutting class.

---
+++ PipeCutterTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class PipeCutterTool : Tool
+{
+    public PipeCutterTool(string name, string weight) : base(name, weight)
+    {
+        this._name = name;
+        this._weight = weight;
+    }
+
+    public override void Describe()
+    {
+        Console.WriteLine($"The name of the current is:{_name} tool is and its weight is: {_weight}");
+
+    }
+
+
+    public override void Use()
+    {
+        Console.WriteLine($"{_name}  Used for cutting pipes ");
+
+    }
+}
