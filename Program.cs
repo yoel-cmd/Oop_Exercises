f87334ab531340916b9ed31a52444bf3ca5b9101
yoel <yoeli5746@gmail.com>
2025-05-19 17:19:26 +0300

    I implemented 3 main tools, added them to the list and ran the methods on each of them.

--- Program.cs
+++ Program.cs
@@ -13,10 +13,24 @@ namespace Oop_Exercises
             Console.WriteLine("hi");
 
 
-            WrenchTool wrench = new WrenchTool("wrench", "3");
+            //WrenchTool wrench = new WrenchTool("wrench", "3");
 
-            wrench.Use();
-            wrench.Describe();
+            DrillTool drill1 = new DrillTool("drill 1", "3.5");
+            DrillTool drill2 = new DrillTool("drill 2", "4.4");
+            HammerTool hammer1 = new HammerTool("hammer 1", "7.5");
+
+            List<Tool> listTool = new List<Tool>();
+            listTool.Add(drill1);
+            listTool.Add(drill2);
+            listTool.Add(hammer1);
+
+            foreach (Tool item in listTool)
+            {
+                item.Describe();
+                item.Use();
+            }
+
+            
 
         }
     }
